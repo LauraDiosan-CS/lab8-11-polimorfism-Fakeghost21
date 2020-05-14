584c672f8b2ca617b91d7e0388556341c7584e3b
Fakeghost21 <62256190+Fakeghost21@users.noreply.github.com>
2020-05-14 13:12:18 +0300

    Update RepoFile.h

--- RepoFile.h
+++ RepoFile.h
@@ -1,50 +1,91 @@
 #pragma once
-#include "ArrayContainer.h"
-#include <iostream>
-#include <fstream>
-#include <string.h>
+#include <vector>
 using namespace std;
-
-class RepoGeneral {
+template <class T>
+class Repository {
 protected:
-	IContainer* container;
+	vector<T*> entity;
 public:
-	RepoGeneral(IContainer* e)
-	{
-		this->container = e;
-	}
+	Repository();
+	~Repository();
 
-	~RepoGeneral()
-	{
-	}
-	
-	virtual IE** getALL()
-	{
-		return this->container->getALL();
-	}
+	virtual void clear();
+	virtual void add(T* p);
+	virtual vector<T*> getAll();
+	virtual void update(T* old, T* newElem);
+	virtual int getSize();
+	virtual T* getPosEntity(int pos);
+	virtual void saveToFile() = 0;
+	virtual void loadFromFile() = 0;
+	virtual void remove(int id);
 
-	virtual IE* getElemPos(int pos) {
-		return this->container->getElemPos(pos);
-	}
-
-	virtual void add(IE* e)
-	{
-		this->container->addElem(e);
-	}
-
-	virtual void del(IE* e)
-	{
-		this->container->removeElem(e);
-	}
-
-	virtual int getSize()
-	{
-		return this->container->getSize();
-	}
-
-	virtual void update(IE* oldElem, IE* newElem)
-	{
-		this->container->updateElem(oldElem,newElem);
-	}
-	
 };
+
+template <class T> Repository<T>::Repository() {
+}
+
+template <class T> void Repository<T>::clear()
+{
+	this->entity.clear();
+	this->saveToFile();
+}
+
+template <class T>Repository<T>::~Repository()
+{
+	this->entity.clear();
+}
+
+template <class T> void Repository<T>::add(T* t)
+{
+	this->entity.push_back(t->clone());
+	this->saveToFile();
+}
+
+template <class T> vector<T*> Repository<T>::getAll()
+{
+	return this->entity;
+}
+
+template<class T>
+inline void Repository<T>::update(T* old, T* newElem)
+{
+	for (int i = 0; i < this->entity.size(); i++)
+	{
+		if ((*this->entity[i]) == *old)
+		{
+			delete this->entity[i];
+			this->entity[i] = newElem->clone();
+		}
+	}
+	this->saveToFile();
+}
+
+template <class T> int Repository<T>::getSize()
+{
+	return this->entity.size();
+}
+
+template <class T> T* Repository<T>::getPosEntity(int pos)
+{
+	return this->entity[pos];
+}
+
+
+
+template<class T>
+inline void Repository<T>::remove(int id)
+{
+	T* p;
+	int i = 0;
+	while(i < this->entity.size() and this->entity[i]->getId() != id)
+	{
+		i++;
+	}
+	if (i < this->entity.size())
+	{
+		delete this->entity[i];
+		this->entity.erase(this->entity.begin() + i);
+		this->saveToFile();
+	}
+
+}
